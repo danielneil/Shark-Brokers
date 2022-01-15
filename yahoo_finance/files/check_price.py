4c1bb4a8ed3790cf2d368aa0c7a0b1b75210fe5c
Daniel Neil <danielneilhodder@gmail.com>
2022-01-15 16:06:55 +1100

    Update check_price.py

--- yahoo_finance/files/check_price.py
+++ yahoo_finance/files/check_price.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     ticker = args.ticker
     
-    history_dir = "/shark/historical/"yahoo_finance_data" + "/" + ticker + ".csv"
+    datafile = "/shark/historical/yahoo_finance_data/" + ticker + ".csv"
         
     try:
         f = open(datafile)
